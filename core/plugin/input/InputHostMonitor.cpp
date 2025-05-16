69394e18d08e73a2a5b72a4cc320216b8f5d768e
wardenjohn <33785009+wardenjohn@users.noreply.github.com>
2025-05-16 16:16:22 +0800

    (Bugfix)InputHostMonitor.cpp:Add mIntervals to adjust the collection interval when more collector is needed (#2206)
    When a new collector is registerd, missing interval will make loongcollector stuck.
    Adding a new vector to store the mInterval of each collector, to make sure
    each collector have its own interval instead of 0, making loongcollector stuck.

--- core/plugin/input/InputHostMonitor.cpp
+++ core/plugin/input/InputHostMonitor.cpp
@@ -139,7 +139,7 @@ bool InputHostMonitor::Init(const Json::Value& config, Json::Value& optionalGoPi
 bool InputHostMonitor::Start() {
     HostMonitorInputRunner::GetInstance()->Init();
     HostMonitorInputRunner::GetInstance()->UpdateCollector(
-        mCollectors, mIntervals, mContext->GetProcessQueueKey(), mIndex);
+        mCollectors, std::vector(mCollectors.size(), mInterval), mContext->GetProcessQueueKey(), mIndex);
     return true;
 }
 
