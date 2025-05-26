5c3494609304024f886bb6d263817244118fc8e0
Wardenjohn <zhangwarden@gmail.com>
2025-05-26 19:31:48 +0800

    Add MemCollector
    Add MemCollector UnitTest

--- core/host_monitor/HostMonitorInputRunner.cpp
+++ core/host_monitor/HostMonitorInputRunner.cpp
@@ -36,6 +36,8 @@
 #include "host_monitor/HostMonitorTimerEvent.h"
 #include "host_monitor/collector/CPUCollector.h"
 #include "host_monitor/collector/ProcessEntityCollector.h"
+//#include "host_monitor/collector/SystemCollector.h"
+#include "host_monitor/collector/MemCollector.h"
 #include "logger/Logger.h"
 #include "models/MetricEvent.h"
 #include "models/PipelineEventGroup.h"
@@ -53,6 +55,8 @@ namespace logtail {
 HostMonitorInputRunner::HostMonitorInputRunner() {
     RegisterCollector<ProcessEntityCollector>();
     RegisterCollector<CPUCollector>();
+    //RegisterCollector<SystemCollector>();
+    RegisterCollector<MemCollector>();
 
     size_t threadPoolSize = 1;
     // threadPoolSize should be greater than 0
